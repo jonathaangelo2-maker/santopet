65c56cd5af4ab71d468a839a8217d2b28542039c
jonathaangelo2-maker <jonathaangelo2@gmail.com>
2026-06-02 22:40:54 -0300

    Atualiza URL do GAS para nova implantação

--- frontend/js/api.js
+++ frontend/js/api.js
@@ -3,7 +3,7 @@
 // ============================================================
 
 var API = (function() {
-  var GAS_URL = 'https://script.google.com/macros/s/AKfycbwkV_d8hnFLaCdVeaXh1PjTjT6UerEMiikbz3EsuwNH5Xg6mFDH_5I9dRZiqAnWbmX-9A/exec'; // <<< Preencher após publicar o GAS
+  var GAS_URL = 'https://script.google.com/macros/s/AKfycbwimfqp4BD9NBPP222zXZrpJgbAdVS6aQ3DEJOX_fpcXnzqskOkRcwAeowKwNR4DzBDXQ/exec'; // <<< Preencher após publicar o GAS
 
   function getToken() {
     return sessionStorage.getItem('sp_token') || '';
